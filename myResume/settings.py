47c5b1baf80e4c8d26a23dc0aba26ba9c83ff331
bameethanida <bameejrn@outlook.com>
2021-01-12 22:06:05 +0700

    add .gitignore

--- myResume/settings.py
+++ myResume/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -119,8 +119,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/statics/'
+STATIC_URL = '/staticfiles/'
+MEDIA_URL = '/images/'
 
-STATIC_DIRS = [
-    os.path.join(BASE_DIR, 'statics')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'staticfiles')
 ]
