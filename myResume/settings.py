a52febba56b2feddf523d71723e094bb034a8f90
bameethanida <bameejrn@outlook.com>
2021-01-19 15:47:27 +0700

    Add allowedhost

--- myResume/settings.py
+++ myResume/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 't!i3s1*9b2*&n8*iz0$_i)+#jf7%41(6nma$#i^ql(7ww=8fd('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['bameethanida.pythonanywhere.com']
 
 
 # Application definition
